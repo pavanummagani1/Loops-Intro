5313e8015c3ccff98d6a704b629a9f6a1ead2365
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-11-26 20:04:41 +0530

    Loops Intro

---
+++ script.js
@@ -0,0 +1,44 @@
+//From 1 to 20
+for(var i = 1; i<=20; i++){
+    console.log(i);
+}
+
+//Squares from 1 to 10
+
+for(var i = 1; i<=10; i++){
+    console.log(i**2);
+}
+
+//first 10 Multiples of 5
+
+for(var i = 1; i<=10; i++){
+    console.log(i*5);
+}
+
+//From 20 to 0
+for(var i = 20; i>=0; i--){
+    console.log(i);
+}
+
+//Even numbers from 20 t0 0
+for(var i = 20; i>=0; i--){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+
+//Array
+arr = [10,20,30,40,50]
+for(var i =0; i<=(arr.length-1); i++){
+    console.log(arr[i]);
+}
+
+
+
+array = ['apple','banana','cherry']
+for(var j =0; j<=(array.length-1); j++){
+    console.log(`index:${j}`,array[j]);
+}
+
+
