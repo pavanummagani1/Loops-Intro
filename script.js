ea595f5451f9fff67d486bb8a1b062d2f32a9ce8
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-11-26 20:21:46 +0530

    LOOPS INTRO

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ for(var i = 1; i<=20; i++){
     console.log(i);
 }
 
-//Squares from 1 to 10
 
+//Squares from 1 to 10
 for(var i = 1; i<=10; i++){
     console.log(i**2);
 }
@@ -15,18 +15,20 @@ for(var i = 1; i<=10; i++){
     console.log(i*5);
 }
 
+
 //From 20 to 0
+
 for(var i = 20; i>=0; i--){
     console.log(i);
 }
 
 //Even numbers from 20 t0 0
+
 for(var i = 20; i>=0; i--){
     if(i%2==0){
         console.log(i);
     }
 }
-
 
 //Array
 arr = [10,20,30,40,50]
@@ -35,10 +37,7 @@ for(var i =0; i<=(arr.length-1); i++){
 }
 
 
-
 array = ['apple','banana','cherry']
 for(var j =0; j<=(array.length-1); j++){
     console.log(`index:${j}`,array[j]);
 }
-
-
