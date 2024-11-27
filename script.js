93535543c12c3a4139681f4a2c462ef84cf0282c
PavanUmmagani07 <pavankalyanU@outlook.com>
2024-11-27 10:07:48 +0530

    LOOPS INTRO

--- script.js
+++ script.js
@@ -1,43 +1,61 @@
-//From 1 to 20
+//1. Write a for loop to print the numbers from 1 to 20.
 for(var i = 1; i<=20; i++){
     console.log(i);
 }
 
 
-//Squares from 1 to 10
+//2. Create a for loop to print the squares of numbers from 1 to 10.
 for(var i = 1; i<=10; i++){
     console.log(i**2);
 }
 
-//first 10 Multiples of 5
-
+//3. Use a for loop to print the first 10 multiples of 5.
 for(var i = 1; i<=10; i++){
     console.log(i*5);
 }
 
 
-//From 20 to 0
-
+//4. Write a for loop to print numbers from 20 to 1 in reverse order.
 for(var i = 20; i>=0; i--){
     console.log(i);
 }
 
-//Even numbers from 20 t0 0
-
+//5. Create a for loop to print the first 10 even numbers in reverse order (e.g., 20, 18, 16, …).
 for(var i = 20; i>=0; i--){
     if(i%2==0){
         console.log(i);
     }
 }
 
-//Array
+/*
+6.	Print each element of the array
+–	Input: [10, 20, 30, 40, 50]
+
+–	Expected Output:
+ 	10  
+    20  
+    30  
+    40  
+    50  
+*/
 arr = [10,20,30,40,50]
 for(var i =0; i<=(arr.length-1); i++){
     console.log(arr[i]);
 }
 
 
+/*
+7.	Print the index of each element along with its value
+–	Input: ['apple', 'banana', 'cherry']
+
+–	Expected Output:
+ 	Index 0: apple  
+    Index 1: banana  
+    Index 2: cherry  
+
+*/
+
 array = ['apple','banana','cherry']
 for(var j =0; j<=(array.length-1); j++){
-    console.log(`index:${j}`,array[j]);
+    console.log(`Index ${j}:`,array[j]);
 }
